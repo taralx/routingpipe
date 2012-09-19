9c6b9ff7fcfdc9dd05f7c11ee125de0480087cb7
JP Sugarbroad <taral@taral.net>
2012-09-18 20:42:51 -0700

    Version 1.1.0

--- src/net/taral/mc/routingpipe/Main.java
+++ src/net/taral/mc/routingpipe/Main.java
@@ -26,7 +26,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "RoutingPipe", version = "1.0.1", name = "Routing Pipe", dependencies = "required-after:BuildCraft|Transport")
+@Mod(modid = "RoutingPipe", version = "1.1.0", name = "Routing Pipe", dependencies = "required-after:BuildCraft|Transport")
 @NetworkMod(clientSideRequired = true)
 public class Main {
 	public static final int GUI_ID = 1;
